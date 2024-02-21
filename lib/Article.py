6c868e727006745861f650e8c20dc4c6ea14bb62
Joseph <joseph.dena@student.moringaschool.com>
2024-02-21 11:19:13 +0300

    correction commit

--- lib/Article.py
+++ lib/Article.py
@@ -1,26 +1,27 @@
 # article.py
 class Article:
-    # Class variable to store all articles
     all_articles = []
 
     def __init__(self, author, magazine, title):
-        # Constructor to initialize article attributes
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        # Add the article to various lists
+        self._author = author
+        self._magazine = magazine
+        self._title = title
         self.__class__.all_articles.append(self)
         magazine.published_articles.append(self)
         author.authored_articles.append(self)
 
     @property
-    def article_title(self):
-        return self.title
+    def title(self):
+        return self._title
 
     @property
-    def author_name(self):
-        return self.author.name
+    def author(self):
+        return self._author
 
     @property
-    def magazine_name(self):
-        return self.magazine.name
+    def magazine(self):
+        return self._magazine
+
+    @classmethod
+    def all(cls):
+        return cls.all_articles
